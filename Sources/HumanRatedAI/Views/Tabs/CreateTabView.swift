44d98f61d4e09e056fae73cc93aa97a76432fa14
Denis Bystruev <dbystruev@me.com>
2024-10-27 13:15:21 +0400

    Tabs: separate CreateTabView

---
+++ Sources/HumanRatedAI/Views/Tabs/CreateTabView.swift
@@ -0,0 +1,18 @@
+// This is free software: you can redistribute and/or modify it
+// under the terms of the GNU General Public License 3.0
+// as published by the Free Software Foundation https://fsf.org
+
+//
+//  CreateTabView.swift
+//  human-rated-ai
+//
+//  Created by Denis Bystruev on 10/27/24.
+//
+
+import SwiftUI
+
+struct CreateTabView: View {
+    var body: some View {
+        Text("Create AI Bot")
+    }
+}
